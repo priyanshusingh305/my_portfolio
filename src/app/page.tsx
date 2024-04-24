3709d6e68fed3726b05ab14ef5db3e7e1771f868
Priyanshu Singh <singhpriyanshu305@gmail.com>
2024-04-24 18:06:26 +0530

    spotlight

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,15 +4,10 @@ import { Spotlight } from "@/components/ui/Spotlight";
 
 export default function Home() {
 	return<div>
-	<div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black dark: bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
-	<div className="h-full w-full dark:bg-black bg-white  dark:bg-dot-white/[0.2] bg-dot-black/[0.2] relative flex items-center justify-center">
-		<div className=" h-full w-screen">
-		<Spotlight className="-top-40 left-0 md:left-60 md:-top-20 " fill="white" />
-		<Spotlight className="-top-40 left-0 md:left-60 md:-top-20 dark:hidden -z-1" fill="#6a00ff" />
+
 
 			<HomePage />
 		<About/>
-		</div>
-	</div>
+	
 </div>
 }
